ac4f1413ee204fca92203686d8446390545df53d
origin <dubnitskaya.katya@mail.ru>
2026-01-26 15:50:08 +0400

    new1

--- src/org/skypro/skyshop/App.java
+++ src/org/skypro/skyshop/App.java
@@ -7,40 +7,40 @@ public class App {
     public static void main(String[] args) {
         ProductBasket basket = new ProductBasket();
 
-        // Добавление продукта
+
         Product bread = new Product("Хлеб", 50);
         basket.add(bread);
 
-        // Заполняем корзину полностью
+
         for (int i = 0; i < 4; i++) {
             basket.add(new Product("Товар" + i, 100));
         }
 
-        // Добавление в заполненную корзину
-        basket.add(new Product("Новый товар", 200));  // Выведет "Невозможно добавить продукт"
 
-        // Печать с товарами
+        basket.add(new Product("Новый товар", 200));
+
+
         basket.print();
 
-        // Получение стоимости
+
         System.out.println("Стоимость: " + basket.getTotalPrice());
 
-        // Поиск существующего
+
         System.out.println("Есть Хлеб: " + basket.contains("Хлеб"));
 
-        // Поиск несуществующего
+
         System.out.println("Есть Молоко: " + basket.contains("Молоко"));
 
-        // Очистка
+
         basket.clear();
 
-        // Печать пустой
+
         basket.print();
 
-        // Стоимость пустой
+
         System.out.println("Стоимость: " + basket.getTotalPrice());
 
-        // Поиск в пустой
+
         System.out.println("Есть Хлеб: " + basket.contains("Хлеб"));
     }
 }
