f48e99a53336c1159b5c630a577f1c707034e51f
origin <dubnitskaya.katya@mail.ru>
2026-01-31 15:20:27 +0400

    new1

--- src/org/skypro/skyshop/basket/ProductBasket.java
+++ src/org/skypro/skyshop/basket/ProductBasket.java
@@ -7,51 +7,56 @@ public class ProductBasket {
     private int count;
 
     public void add(Product product) {
-            if (count >= products.length) {
+        boolean added = false;
+        for (int i = 0; i < products.length; i++) {
+            if (products[i] == null) {
+                products[i] = product;
+                added = true;
+                break;
+            }
+        }
+        if (!added) {
             System.out.println("Невозможно добавить продукт");
-            return;
         }
-        products[count] = product;
-        count++;
     }
-
-    public int getTotalPrice() {
-        int total = 0;
-        for (int i = 0; i < count; i++) {
-            if (products[i] != null) {
-                total += products[i].getPrice();
+        public int getTotalPrice () {
+            int total = 0;
+            for (int i = 0; i < count; i++) {
+                if (products[i] != null) {
+                    total += products[i].getPrice();
+                }
             }
+            return total;
         }
-        return total;
-    }
 
-    public void print() {
-        if (count == 0) {
-            System.out.println("в корзине пусто");
-            return;
-        }
-        for (int i = 0; i < count; i++) {
-            System.out.println(products[i].getName() + ": " + products[i].getPrice());
-        }
-        System.out.println("Итого: " + getTotalPrice());
-    }
-
-    public boolean contains(String name) {
-        for (int i = 0; i < count; i++) {
-            if (products[i].getName().equals(name)) {
-                return true;
+        public void print () {
+            if (count == 0) {
+                System.out.println("в корзине пусто");
+                return;
             }
+            for (int i = 0; i < count; i++) {
+                System.out.println(products[i].getName() + ": " + products[i].getPrice());
+            }
+            System.out.println("Итого: " + getTotalPrice());
         }
-        return false;
+
+        public boolean contains (String name){
+            for (int i = 0; i < count; i++) {
+                if (products[i].getName().equals(name)) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public void clear () {
+            for (int i = 0; i < count; i++) {
+                products[i] = null;
+            }
+            count = 0;
+        }
     }
 
-    public void clear() {
-        for (int i = 0; i < count; i++) {
-            products[i] = null;
-        }
-        count = 0;
-    }
-}
 
 
 
